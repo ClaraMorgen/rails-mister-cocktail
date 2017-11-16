5738e3ad76fa5d57560b5a0110b9ca34fd2182ec
Clara Morgeneyer <clara_morgeneyer@web.de>
2017-11-16 12:06:48 +0100

    Routes added

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
-  get 'doses/new'
-
-  get 'cocktails/new'
-
-  get 'cocktails/show'
-
-  get 'cocktails/index'
+ resources :cocktails, only: [:index, :new, :show, :create] do
+  resources :dose, only: [:new, :create, :destroy]
+ end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
