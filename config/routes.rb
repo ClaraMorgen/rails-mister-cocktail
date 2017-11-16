e9a685649a09a97d7e3f889c32865f3d4e2c6c44
Clara Morgeneyer <clara_morgeneyer@web.de>
2017-11-16 12:28:17 +0100

    Break

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
 
 resources :doses, only: [:destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-root :to 'cocktails#index'
+root to: 'cocktails#index'
 end
